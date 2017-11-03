667d9c330d19e5ca9dc058fcd886f623ff4d2190
Braagaa <michael_braga@outlook.com>
2017-11-03 18:05:03 -0400

    Working API call to randomuser.me
    -Added native helper functions
    -Able to form query string with param Object

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-const getJSON = function(url, param) {
+/*
+* Similar to Array.prototype.reduce but used to iterate over an Object's
+* key and values
+*/
+if (!Object.prototype.reduce) { 
+	Object.prototype.reduce = function(fn, acc) {
+		for (let prop in this) {
+			if (this.hasOwnProperty(prop)) {
+				acc = fn(acc, this[prop], prop, this);
+			}
+		}
+		return acc;
+	}
+}
+/*
+* Inserts another String to a position to the current String
+*/
+if (!String.prototype.insert) {
+	String.prototype.insert = function(value, i) {
+		value = typeof value === 'string' ? value : '';
+		const clone = this.slice(0, this.length).split('');
+		clone.splice(0, i, value)
+		return clone.join('');
+	}
+}
+
+const getJSON = function(url) {
 	return new Promise(function(resolve) {
 		const xhr = new XMLHttpRequest();
 		xhr.open('GET', url, true);
@@ -11,3 +37,19 @@ const getJSON = function(url, param) {
 		xhr.send();
 	});
 }
+
+const randomUserAPI = 'https://randomuser.me/api/?';
+const params = {
+	results: 12,
+	format: 'json',
+	exc: 'login'
+}
+
+const query = params
+	.reduce((query, value, key) => [...query, `${key}=${value}`], [])
+	.map(encodeURI)
+	.join('&')
+	.insert(randomUserAPI, 0, 0);
+	
+getJSON(query)
+	.then(console.log);
